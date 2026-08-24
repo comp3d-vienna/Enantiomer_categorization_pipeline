d79f43893721b98fdb890fca21c0e0236d06371d
Huanni Zhang <huanni.zhang@univie.ac.at>
2026-08-24 16:52:50 +0200

    remove src

--- src/enantiomer_binding_conformation/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Documentation about enantiomer_binding_conformation."""
-
-import logging
-
-logging.getLogger(__name__).addHandler(logging.NullHandler())
-
-__author__ = "Huanni Zhang"
-__email__ = "huanni.zhang@univie.ac.at"
-__version__ = "3.13.0"
